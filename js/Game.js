212fff8245fa56bbd7caf6641c32c68f23aa89f1
Joseph Doucet <sssd09@gmail.com>
2020-06-14 13:25:20 -0400

    added in phrase and game classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,13 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+/**The class for the game object
+ * Tracks missed letters and handles what phrase is being guessed
+ */
+class Game {
+    constructor() {
+        //The number of incorrect guesses
+        this.missed = 0;
+        //An array of phrases to be used in the game
+        this.phrase = [];
+        //The phrase being guessed in the current game
+        this.activePhrase = null;
+    }
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,20 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+//Handles the createion of the phrases
+class Phrase {
+    /**Creating the phrase to be guessed
+     * @param {string} phrase The phrase to be guessed
+     */
+    constructor(phrase){
+        //the phrase is stored, in lowercase, in the phrase property
+        this.phrase = phrase.toLowerCase();
+    }
+    
+    /**Displays the phrase in the game
+     * @param {string} phrase The phrase that is going to be displayed in game
+     */
+    addPhraseToDisplay(phrase) {
+        const phraseArr = phrase.split('');
+        phraseArr.forEach(letter => {
+            document.getElementById('phrase').firstElementChild.innerHTML = `<li class="hide letter ${letter}">${letter}</li>`;
+        })
+    }
+ }
