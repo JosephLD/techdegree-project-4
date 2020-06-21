03c7202823dc4404a7c29379ea64b4074de74e9f
Joseph Doucet <sssd09@gmail.com>
2020-06-21 19:03:49 -0400

    Initialized game from apps.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-const game = new Game();
-game.startGame();
+let game;
+//Adding an event listener to the start button
+document.getElementById('btn__reset').addEventListener('click', (e) => {
+    //Here I initiate a new game object
+    game = new Game();
+    //and then call the start game method on it to start the game, hide the overlay,
+    //and pick and display a hidden phrase
+    game.startGame();
+})
