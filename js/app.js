11f725856d4f9a8209cfb4dc03f4116fd5991d78
Joseph Doucet <sssd09@gmail.com>
2020-06-21 19:06:36 -0400

    Added comments
    Added a few comments to the apps.js file for clarification

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+//declaring an empty game variable to initialize the game object later
 let game;
 //Adding an event listener to the start button
 document.getElementById('btn__reset').addEventListener('click', (e) => {
-    //Here I initiate a new game object
+    //Here I initiate a new game object so that each time the button is pressed
+    //a new game is created
     game = new Game();
     //and then call the start game method on it to start the game, hide the overlay,
     //and pick and display a hidden phrase
